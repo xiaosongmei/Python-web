6ed4cfa83c2889958e92c52a485f25f3871e2d87
Tianlan <suxiaomomo@gmail.com>
2018-01-03 21:51:25 +0800

    廖雪峰访问限制作业

--- class.py
+++ class.py
@@ -1,15 +1,29 @@
+#! /usr/bin/python3
+
+
 class Student(object):
-	"""docstring for Studen"""
-	def __init__(self, name, gender):
-		self.name = name
-		self.__gender = gender
-	def get_gender(self):
-		return self.__gender
+    def __init__(self, name, gender):
+        self.name = name
+        self.__gender = gender
 
-	def set_gender(self, gender):
-		if gender == 'male' or gender =='female':
-		self.__gender = gender
-	else:
-		raise ValueError('bad input')
+    def get_gender(self):
+        return self.__gender
 
-		
+    def set_gender(self, gender):
+        if gender == 'male':
+            self.__gender = gender
+        elif gender == 'female':
+            self.__gender = gender
+        else:
+            raise ValueError('error')
+
+
+bart = Student('Bart', 'male')
+if bart.get_gender() != 'male':
+    print('test error')
+else:
+    bart.set_gender('female')
+    if bart.get_gender() != 'female':
+        print('test error')
+    else:
+        print('succesful')
